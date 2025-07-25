49e626ac059c3f39b5e254f5cd21373c3eaf2a92
AustinBev <115735376+AustinBev@users.noreply.github.com>
2025-07-25 16:13:01 -0600

    Overall and main website CSS & custom font Experience page: Work experience and tech stack icons

---
+++ AbevPortfolioCsharp/Layout/MainLayout.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+
+namespace AbevPortfolioCsharp.Layout
+{
+    public partial class MainLayout : LayoutComponentBase
+    {
+        private bool IsDarkMode = true;
+
+        private void SetTheme(string theme)
+        {
+            IsDarkMode = theme == "dark";
+        }
+    }
+}
