c31be5a3490c38cc28756616eca779cda91f721a
AustinBev <115735376+AustinBev@users.noreply.github.com>
2025-07-28 17:35:13 -0600

    Projects added, tech stack -> home page, toggle headshot (pixel/regular).

---
+++ AbevPortfolioCsharp/pages/Home.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AbevPortfolioCsharp.pages
+{
+    public partial class Home : ComponentBase
+    {
+        protected string PixelImage = "/images/PixelAustin.png";
+        protected string RealImage = "/images/ABEV2.jpg";
+        protected string CurrentImage;
+
+        protected override void OnInitialized()
+        {
+            CurrentImage = PixelImage;
+        }
+
+        protected void ToggleHeadshot()
+        {
+            CurrentImage = CurrentImage == PixelImage ? RealImage : PixelImage;
+        }
+    }
+}
