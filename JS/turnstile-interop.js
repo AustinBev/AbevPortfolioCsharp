bba6f64786c1592d32907f0386365e4dc0cd04a7
AustinBev <115735376+AustinBev@users.noreply.github.com>
2025-07-30 18:58:59 +0000

    Deploying to gh-pages from @ AustinBev/AbevPortfolioCsharp@6689ff7eb316217f60594cd0b6da9175c11a5968 🚀

---
+++ JS/turnstile-interop.js
@@ -0,0 +1,8 @@
+﻿window.cfTurnstile = (function () {
+    let token = null;
+    return {
+        onSuccess: function (t) { token = t; },
+        getToken: function () { return token; },
+        reset: function () { if (window.turnstile) window.turnstile.reset(); }
+    };
+})();
