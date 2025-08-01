46b909b08b939a417631792eeb7b090c2d4cd6e9
AustinBev <115735376+AustinBev@users.noreply.github.com>
2025-08-01 20:13:25 +0000

    Deploying to gh-pages from @ AustinBev/AbevPortfolioCsharp@f3e9c2dcff7eccc49ddbf7ef77bf5692d58fe1d1 🚀

--- JS/turnstile-interop.js
+++ JS/turnstile-interop.js
@@ -1,18 +1,20 @@
-﻿// wwwroot/JS/turnstile-interop.js
-// Runs after Turnstile core (script tag already uses defer)
-window.cfTurnstile = {
-    /** Render a widget explicitly */
-    renderWidget: (element, siteKey) =>
-        window.turnstile.render(element, {
-            sitekey: siteKey,
-            theme: 'dark',
-            callback: () => console.log('Turnstile solved')
-        }),
-
-    /** Blazor asks for the current token */
-    getToken: () =>
-        document.querySelector('input[name="cf-turnstile-response"]')?.value || '',
-
-    /** Reset after a successful send */
-    reset: () => window.turnstile?.reset?.()
+﻿window.cfTurnstile = {
+    renderWidget: function (el, sitekey, dotNetObjRef) {
+        window.turnstile.render(el, {
+            sitekey,
+            callback: function (token) {
+                window.cfTurnstile._token = token;
+                if (dotNetObjRef) {
+                    dotNetObjRef.invokeMethodAsync('OnTurnstileReady');
+                }
+            }
+        });
+    },
+    getToken: function () {
+        return window.cfTurnstile._token || "";
+    },
+    reset: function () {
+        window.turnstile.reset();
+        window.cfTurnstile._token = "";
+    }
 };
