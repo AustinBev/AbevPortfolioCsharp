fbea59664f59fa8a2d65730738df287dc26b0f0d
AustinBev <115735376+AustinBev@users.noreply.github.com>
2025-09-03 11:15:24 -0600

    Send button double click fix 1

--- AbevPortfolioCsharp/wwwroot/JS/turnstile-interop.js
+++ AbevPortfolioCsharp/wwwroot/JS/turnstile-interop.js
@@ -1,20 +1,38 @@
-﻿window.cfTurnstile = {
-    renderWidget: function (el, sitekey, dotNetObjRef) {
-        window.turnstile.render(el, {
-            sitekey,
-            callback: function (token) {
-                window.cfTurnstile._token = token;
-                if (dotNetObjRef) {
-                    dotNetObjRef.invokeMethodAsync('OnTurnstileReady');
-                }
+﻿//window.cfTurnstile = {
+//    renderWidget: function (el, sitekey, dotNetObjRef) {
+//        window.turnstile.render(el, {
+//            sitekey,
+//            callback: function (token) {
+//                window.cfTurnstile._token = token;
+//                if (dotNetObjRef) {
+//                    dotNetObjRef.invokeMethodAsync('OnTurnstileReady');
+//                }
+//            }
+//        });
+//    },
+//    getToken: function () {
+//        return window.cfTurnstile._token || "";
+//    },
+//    reset: function () {
+//        window.turnstile.reset();
+//        window.cfTurnstile._token = "";
+//    }
+//};
+
+// wwwroot/JS/turnstile-interop.js - newcd
+window.cfTurnstile = {
+    renderWidget: (element, siteKey, dotNetObjRef) =>
+        window.turnstile.render(element, {
+            sitekey: siteKey,
+            theme: 'dark',
+            callback: () => {
+                console.log('Turnstile ready');
+                dotNetObjRef.invokeMethodAsync('OnTurnstileReady');
             }
-        });
-    },
-    getToken: function () {
-        return window.cfTurnstile._token || "";
-    },
-    reset: function () {
-        window.turnstile.reset();
-        window.cfTurnstile._token = "";
-    }
+        }),
+
+    getToken: () =>
+        document.querySelector('input[name="cf-turnstile-response"]')?.value || '',
+
+    reset: () => window.turnstile?.reset?.()
 };
